cb1ef225eb9bc6bbc74968f2a326c25f333f7bb4
manasarora14 <manas.arora142@gmail.com>
2025-12-18 20:11:54 +0530

    Code

--- student-app/src/main.ts
+++ student-app/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';             // <-- Use app.ts (not app.component)
+import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
@@ -10,4 +10,4 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes),
     importProvidersFrom(HttpClientModule)
   ]
-});
+}).catch(err => console.error(err));
